518babae8a22ac82a14122d32980509656f0772b
Matt Richardson <matt.richardson@octopus.com>
2020-06-09 14:00:31 +1000

    first commit

---
+++ test-octopus-build-information/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_octopus_build_information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
